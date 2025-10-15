527f8ef68689d5769ed4c04503df2bfbd06fdd84
Juan Hernandez <juandanihv@gmail.com>
2025-10-15 17:38:21 -0500

    relationships

--- TABLES/01_create_table.sql
+++ TABLES/01_create_table.sql
@@ -52,3 +52,6 @@ CREATE TABLE personas6 (
 
 
 
+
+
+
---
+++ TABLES/04_relationships.sql
@@ -0,0 +1,21 @@
+CREATE TABLE dni (
+	dni_id int AUTO_INCREMENT PRIMARY KEY, 
+    dni_number int NOT NULL, 
+    user_id int,
+    UNIQUE (dni_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) 
+); 
+
+
+CREATE TABLE companies (
+	compny_id int AUTO_INCREMENT PRIMARY KEY, 
+    name varchar (100) NOT NULL
+);
+
+ALTER TABLE users
+ADD company_id int,
+ADD FOREIGN KEY (company_id) REFERENCES companies (compny_id);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_companies
+FOREIGN KEY (company_id) REFERENCES companies (company_id)
