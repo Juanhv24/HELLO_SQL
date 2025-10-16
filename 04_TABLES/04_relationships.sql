706731e24f5b2af9295c9205282f3870ad9cac88
Juan Hernandez <juandanihv@gmail.com>
2025-10-15 21:11:01 -0500

    V3

---
+++ 04_TABLES/04_relationships.sql
@@ -0,0 +1,37 @@
+-- Relación 1:1 entre usuarios y DNI
+CREATE TABLE dni (
+	dni_id int AUTO_INCREMENT PRIMARY KEY, 
+    dni_number int NOT NULL, 
+    user_id int,
+    UNIQUE (dni_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) 
+); 
+
+-- Relación 1:N entre usuarios y empresas
+CREATE TABLE companies (
+	company_id int AUTO_INCREMENT PRIMARY KEY, 
+    name varchar (100) NOT NULL
+);
+
+ALTER TABLE users
+ADD company_id int,
+ADD FOREIGN KEY (company_id) REFERENCES companies (compny_id);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_companies
+FOREIGN KEY (company_id) REFERENCES companies (company_id)
+
+-- Relación N:M entre usuarios y lenguajes
+CREATE TABLE languages (
+	language_id int AUTO_INCREMENT PRIMARY KEY, 
+    name varchar (100) NOT NULL
+);
+
+CREATE TABLE users_languages (
+	users_languages_id int AUTO_INCREMENT PRIMARY KEY,
+    user_id int, 
+    language_id int,
+    FOREIGN KEY (user_id) REFERENCES users (user_id), 
+    FOREIGN KEY (language_id) REFERENCES languages (language_id),
+    UNIQUE (user_id, language_id)
+);
